5a105417fca3dbbcfb57b2c2d007c2dc0dd25bf1
ChaeYeChan <62997897+ChaeYeChan@users.noreply.github.com>
2021-06-04 01:49:52 +0900

    2021-06-04
    ToDoList_jsp
    HTML, CSS, JSP

---
+++ bg.js
@@ -0,0 +1,32 @@
+// const body = document.querySelector("body");
+
+// // const IMG_NUMBER = 4;
+
+// // function paintImage(imgNumber){
+// //     const image = new Image();
+// //     image.src = `images/${imgNumber + 1}.jpg`;
+// //     image.classList.add("bgImage");
+// //     body.appendChild(image);
+// //     //loadend
+// // }
+
+// // function genRandom(){
+// //     const number = Math.floor(Math.random() * IMG_NUMBER);
+// //     return number;
+// // }
+
+// // function init(){
+// //     const randomNumber = genRandom();
+// //     paintImage(randomNumber);
+// // }
+
+// function image(){
+//     const image = new Image();
+//     image.src = `images/${hannam_gif}.gif`;
+// }
+
+// function init(){
+//     image();
+// }
+
+// init();
